aa3761581765e43634b1675b67cb0ceed755c3c4
Dequisa <Dequisas@gmail.com>
2021-06-12 18:14:10 -0400

    added conditional statement

--- app.js
+++ app.js
@@ -4,18 +4,26 @@ const app = express();
 app.get("/bugs", (request, response, next)=>{
     response.send(`  
     <h1>99 little bugs in the code</h1>
-    <a href= "/bugs/$num" >pull one down, patch it around</a>
+    <a href= "/bugs/101" >pull one down, patch it around</a>
   `);
     
 });
 
 const num =0;
-app.get("/bugs/:101", (request, response)=>{
+app.get("/bugs/:num", (request, response)=>{
     console.log(request.params.num)
+    if(request.params.num > 200){
+        response.send(`  
+        <h1>  ${request.params.num} little bugs in the code</h1>
+        <a href= "/bugs " >Your code crashed</a>
+      `);
+        
+    }else
     response.send(`  
-    <h1>  ${request.params.101} little bugs in the code</h1>
-    <a href= "/bugs/${request.params.num+1} " >pull one down, patch it around</a>
+    <h1>  ${request.params.num} little bugs in the code</h1>
+    <a href= "/bugs/${parseInt(request.params.num)+1} " >pull one down, patch it around</a>
   `);
+
 });
 
 app.get("/bugs/:id/:id", (request, response)=>{
